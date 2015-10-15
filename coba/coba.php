9ed279c5ec875c6f035003a0d77ba54522c6b75d
Chandra Putra Wijaya <chandraputrawijaya@hotmail.com>
2015-10-15 14:09:20 +0700

    add1

--- coba/coba.php
+++
@@ -1,7 +0,0 @@
-<!-- name input -->
-<input type="text" id="name" name="name" value="Jonny Dough" />
-
-<script>
-var name = document.getElementById("name");
-if (name.value != name.defaultValue) alert("#name has changed");
-</script>
