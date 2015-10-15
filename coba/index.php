690cf3d45c9214708a5261710e9ff61bc255c376
Chandra Putra Wijaya <chandraputrawijaya@hotmail.com>
2015-10-15 14:07:39 +0700

    add

--- coba/index.php
+++ coba/index.php
@@ -2,9 +2,9 @@
 
 <?php
 
-$a = 'b';
+$a = true;
 
-echo empty($a) ? 'a' : "Benar";
+echo $a ? 'a' : "Benar";
 
 echo '<br />';
 
@@ -13,3 +13,46 @@ echo '<br />';
 $var = 3;
 $t = ($var > 2 ? 'Var lebih besar' : 'Var lebih kecil'); // returns true
 echo $t;
+echo '<br />';
+
+$score = 70;
+$age = 10;
+echo 'Tentang umur: ',
+($age > 10 ? ($score < 80 ? 'behind 80' : 'above average 80') : ($score < 50 ? 'behind 50' : 'above average 50'));
+
+echo '<br />';
+
+if ($age > 10){
+	if($score < 80){
+		echo 'behind 80';
+	} else {
+		echo 'above average 80';
+	}
+} else{
+	if($score < 50){
+		echo 'behind 50';
+	} else {
+		echo 'above average 50';
+	}
+}
+
+$nilai = 85;
+echo '<br />';
+echo 'nilai kamu : ';
+$hasil = ($nilai != null ? ($nilai > 0 && $nilai < 40 ? 'E' : ($nilai > 40 && $nilai < 50 ? 'D' : 
+($nilai > 50 && $nilai < 70 ? 'C' : ($nilai > 70 && $nilai <= 85 ? 'B' : 'A')))) : 'nilai kosong');
+echo $hasil;
+
+
+/*
+penggunaan ternary
+if($hasil){
+	echo 'benar';
+} else{
+	echo 'salah';
+}
+
+echo ($hasil ? 'benar' : 'salah');
+
+
+*/
